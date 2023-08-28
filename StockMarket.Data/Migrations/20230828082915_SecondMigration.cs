bdbecd3eafcb149cd4614667ad63e9c8da698e65
Arman Hosseini <armanhosseini878787@gmail.com>
2023-08-28 14:21:28 +0330

    Create third migration

---
+++ StockMarket.Data/Migrations/20230828082915_SecondMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockMarket.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    BuyOrderId = table.Column<int>(type: "integer", nullable: false),
+                    SellOrderId = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Quantity = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trades", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trades");
+        }
+    }
+}
---
+++ StockMarket.Data/Migrations/20230828083626_ThirdMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockMarket.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThirdMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Trades_BuyOrderId",
+                table: "Trades",
+                column: "BuyOrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trades_SellOrderId",
+                table: "Trades",
+                column: "SellOrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trades_Orders_BuyOrderId",
+                table: "Trades",
+                column: "BuyOrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trades_Orders_SellOrderId",
+                table: "Trades",
+                column: "SellOrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trades_Orders_BuyOrderId",
+                table: "Trades");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trades_Orders_SellOrderId",
+                table: "Trades");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Trades_BuyOrderId",
+                table: "Trades");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Trades_SellOrderId",
+                table: "Trades");
+        }
+    }
+}
