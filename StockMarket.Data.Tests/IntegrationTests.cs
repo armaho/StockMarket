568b74d88739c798fb79d2c9ff2b505148f580c2
Arman Hosseini <armanhosseini878787@gmail.com>
2023-08-28 11:36:15 +0330

    Added DbContext_Should_Save_Trades_In_Database_Test

--- StockMarket.Data.Tests/IntegrationTests.cs
+++ StockMarket.Data.Tests/IntegrationTests.cs
@@ -10,7 +10,7 @@ public class IntegrationTests
     [Fact]
     public void DbContext_Should_Save_Orders_In_Database_Test()
     {
-        //Arange
+        //Arrange
         var optionsBuilder = new DbContextOptionsBuilder<StockMarketDbContext>();
 
         optionsBuilder.UseNpgsql(@"Host=localhost;Username=postgres;Password=arman1383;Database=StockMarket");
@@ -48,6 +48,42 @@ public class IntegrationTests
             Price = 1500M,
             Quantity = 0M,
             IsCanceled = false
+        });
+    }
+
+    public void DbContext_Should_Save_Trades_In_Database_Test()
+    {
+        //Arrange
+        var optionsBuilder = new DbContextOptionsBuilder<StockMarketDbContext>();
+
+        optionsBuilder.UseNpgsql(@"Host=localhost;Username=postgres;Password=arman1383;Database=StockMarket");
+
+        using var context = new StockMarketDbContext(optionsBuilder.Options);
+        var processor = new StockMarketProcessor(IsMarketOpen: true);
+
+        Order.NextInstanceId = ((context.Orders.Max(order => (int?)order.Id)) ?? -1) + 1;
+
+        var buyOrderId = processor.EnqueueOrder(side: TradeSide.Buy, price: 1500M, quantity: 1M);
+        var sellOrderId = processor.EnqueueOrder(side: TradeSide.Sell, price: 1500M, quantity: 1M);
+
+        var buyOrder = processor.Orders.First(order => (order.Id == buyOrderId));
+        var sellOrder = processor.Orders.First(order => (order.Id == sellOrderId));
+        var createdTrade = processor.Trades[0];
+
+        //Act
+        context.Orders.Add(buyOrder);
+        context.Orders.Add(sellOrder);
+        context.Trades.Add(createdTrade);
+        context.SaveChanges();
+
+        //Assert
+        context.Trades.First(trade => (trade.Id == createdTrade.Id)).Should().BeEquivalentTo(new
+        {
+            Id = createdTrade.Id,
+            SellOrderId = sellOrderId,
+            BuyOrderId = buyOrderId,
+            Price = 1500M,
+            Quantity = 1M
         });
     }
 }
