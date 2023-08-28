38d469f886d7de94072f0a954be39ac8defd17f8
Arman Hosseini <armanhosseini878787@gmail.com>
2023-08-28 15:00:54 +0330

    Implement StockMarketDbContextFixture

---
+++ StockMarket.Data.Tests/StockMarketDbContextFixture.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace StockMarket.Data.Tests;
+
+public class StockMarketDbContextFixture : IDisposable
+{
+    public StockMarketDbContext Context { get; init; }
+
+    public StockMarketDbContextFixture()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<StockMarketDbContext>();
+
+        optionsBuilder.UseNpgsql(@"Host=localhost;Username=postgres;Password=arman1383;Database=StockMarket");
+
+        Context = new(optionsBuilder.Options);
+    }
+
+    public void Dispose()
+    {
+        Context.Dispose();
+    }
+}
