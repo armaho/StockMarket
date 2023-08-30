af30b7e5ec6f9424eeede21c8fd37cdcc5af7d67
Arman Hosseini <armanhosseini878787@gmail.com>
2023-08-30 12:20:00 +0330

    Add BlockingCollection_Add_And_Take_Test()
    A test to learn more about BlockingCollection<T> and Task

--- StockMarket.Domain.Tests/BlockingCollectionTests.cs
+++ StockMarket.Domain.Tests/BlockingCollectionTests.cs
@@ -1,5 +1,37 @@
-﻿namespace StockMarket.Domain.Tests;
+﻿using System.Collections.Concurrent;
+
+namespace StockMarket.Domain.Tests;
 public class BlockingCollectionTests
 {
+    [Fact]
+    public void BlockingCollection_Add_And_Take_Test()
+    {
+        //Arrange
+        using var queue = new BlockingCollection<int>();
+        int sum = 0;
 
+        //Act
+        var producer = Task.Run(() =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                queue.Add(i);
+            }
+
+            queue.CompleteAdding();
+        });
+
+        var consumer = Task.Run(() =>
+        {
+            foreach (var i in queue.GetConsumingEnumerable())
+            {
+                sum += i;
+            }
+        });
+
+        Task.WaitAll(producer, consumer);
+
+        //Assert
+        Assert.Equal(45, sum);
+    }
 }
