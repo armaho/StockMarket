5ca71fae6d4c243af0250a374703df76172f269d
Arman Hosseini <armanhosseini878787@gmail.com>
2023-08-30 15:51:18 +0330

    Add BlockingCollection_With_TaskCompletionSource_Test()

--- StockMarket.Domain.Tests/BlockingCollectionTests.cs
+++ StockMarket.Domain.Tests/BlockingCollectionTests.cs
@@ -1,8 +1,35 @@
-﻿using System.Collections.Concurrent;
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+using Xunit.Abstractions;
 
 namespace StockMarket.Domain.Tests;
 public class BlockingCollectionTests
 {
+    private readonly ITestOutputHelper output;
+
+    public BlockingCollectionTests(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    private struct QueueItem
+    {
+        public int Data { get; private set; }
+        public TaskCompletionSource<int> Completion { get; }
+
+        public QueueItem(int data)
+        {
+            this.Data = data;
+            Completion = new();
+        }
+
+        public void IncreaseData()
+        {
+            this.Data++;
+        }
+    }
+
     [Fact]
     public void BlockingCollection_Add_And_Take_Test()
     {
@@ -33,5 +60,42 @@ public class BlockingCollectionTests
 
         //Assert
         Assert.Equal(45, sum);
+    }
+
+    [Fact]
+    public void BlockingCollection_With_TaskCompletionSource_Test()
+    {
+        //Arrange
+        using BlockingCollection<QueueItem> queue = new();
+        int sum = 0;
+
+        //Act
+        var producer = Task.Run(async () =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var item = new QueueItem(i);
+
+                queue.Add(item);
+
+                sum += await item.Completion.Task;
+            }
+
+            queue.CompleteAdding();
+        });
+
+        var consumer = Task.Run(() =>
+        {
+            foreach (var item in queue.GetConsumingEnumerable())
+            {
+                item.IncreaseData();
+                item.Completion.SetResult(item.Data);
+            }
+        });
+
+        Task.WaitAll(producer, consumer);
+
+        //Assert
+        Assert.Equal(55, sum);
     }
 }
